4f789d13f8f700c4df7d4e151b4e61161257ea56
Bastien-Project <b.thomas30712@gmail.com>
2024-05-31 16:23:13 +0200

    texte et ajustement

--- index.php
+++ index.php
@@ -12,17 +12,21 @@
     <div>
         <div class="aboutText">
             <p>
-                Au sein de notre groupe de passionnés de jardinage, l'idée de créer un site dédié à la garde de plantes a émergé naturellement. Animés par notre amour commun pour la verdure, nous avons constaté les défis auxquels nous étions confrontés chaque fois que nous devions nous absenter, laissant nos plantes sans surveillance.
+                <br>
+                Nous sommes une équipe passionnée de développeurs, de créateurs de contenu et de joueurs dédiée à la création d'expériences de jeu immersives et mémorables. <strong>The Procrastinator</strong> est notre projet phare, né de notre amour pour les jeux vidéo et notre désir de créer quelque chose de vraiment unique.
                 <br><br>
-                Notre équipe, composée de personnes aux compétences diverses, s'est rapidement formée. Des développeurs web compétents, des experts en horticulture chevronnés et des esprits créatifs ont uni leurs forces pour transformer notre vision en réalité. Notre objectif était clair : concevoir une plateforme en ligne qui simplifierait la recherche de gardiens qualifiés pour nos plantes bien-aimées.
+                Chez <strong>The Procrastinator</strong>, notre mission est de divertir et d'inspirer les joueurs en leur offrant une aventure captivante et engageante. Nous croyons que les jeux vidéo sont une forme d'art qui peut raconter des histoires puissantes, susciter des émotions profondes et créer des souvenirs inoubliables.
                 <br><br>
-                Les mois de collaboration intense et de développement ont donné naissance à une plateforme intuitive. Chacun d'entre nous a contribué à la création d'un espace où les utilisateurs peuvent détailler les besoins spécifiques de leurs plantes, et où des gardiens compétents peuvent être trouvés en un clic.
+                Notre équipe est composée de talents diversifiés provenant de différents horizons. Nous avons des développeurs expérimentés, des artistes talentueux, des concepteurs de jeux innovants et des spécialistes du marketing dévoués, et tout cela en une seule personne. Ensemble, nous avons mis nos compétences en commun pour donner vie à <strong>The Procrastinator</strong>.
                 <br><br>
-                Ainsi, notre site est né, une concrétisation de notre passion commune. Il va au-delà d'une simple plateforme en ligne, reflétant notre engagement envers la communauté des amoureux de plantes. C'est notre histoire, une aventure collective visant à simplifier la vie des passionnés de verdure, à créer des liens entre des individus partageant une passion commune, et à offrir une tranquillité d'esprit lorsque nos plantes restent entre de bonnes mains en notre absence. Cette histoire est un témoignage de notre dévouement envers la nature, de notre désir de partager notre amour des plantes avec le monde.
+                <strong> The Procrastinator</strong> est un jeu d'aventure et de stratégie où les joueurs doivent naviguer à travers un monde ouvert et fantastique rempli de défis et de mystères. Le but du jeu est d'aider le protagoniste à surmonter la procrastination en résolvant des énigmes, en accomplissant des quêtes et en prenant des décisions cruciales qui influenceront le cours de l'histoire.
+                <br>
             </p>
         </div>
         <div class="aboutImg">
-            <img src="images/bkg1.jpg" alt="Plante">
+            <a href="https://www.youtube.com/watch?v=rlarCLhzfoU" target="_blank">
+                <img src="images/michel.jpg" alt="Ingénieur informaticien">
+            </a>
         </div>
     </div>
 
@@ -30,21 +34,18 @@
     <div>
 
         <div class="aboutImg">
-            <img src="images/bkg3.jpg" alt="Engagement">
+            <img src="images/engagement.jpg" alt="Engagement">
         </div>
 
         <div class="aboutText">
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales metus in est fermentum, id auctor erat ultricies. Integer aliquet nunc ut felis condimentum, a scelerisque justo viverra. Vivamus at magna sed libero fringilla sollicitudin. Mauris vel magna sit amet erat pharetra efficitur nec sit amet felis. Nullam at erat nec metus suscipit congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi.
+                <br><br>
+                Integer vehicula, arcu at cursus euismod, lectus leo hendrerit eros, nec posuere risus orci vel urna. Nulla vehicula euismod sapien, sit amet bibendum libero. Phasellus sagittis urna sed lectus dignissim, nec convallis ligula pretium. Cras ut lectus sapien. Suspendisse vehicula magna ac sapien convallis, at ultricies magna iaculis. Donec tempus tortor id lectus facilisis fermentum. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer id ligula eu metus dapibus fringilla. Morbi euismod sapien sed ultricies sollicitudin.
+                <br><br>
+                Integer vehicula, arcu at cursus euismod, lectus leo hendrerit eros, nec posuere risus orci vel urna. Nulla vehicula euismod sapien, sit amet bibendum libero. Phasellus sagittis urna sed lectus dignissim, nec convallis ligula pretium. Cras ut lectus sapien. Suspendisse vehicula magna ac sapien convallis, at ultricies magna iaculis. Donec tempus tortor id lectus facilisis fermentum. Nulla facilisi. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                <br><br>
 
-            <p>
-                Notre engagement envers la communauté des amoureux de plantes est le cœur battant de notre site. En créant cette plateforme, nous nous sommes engagés à construire bien plus qu'un simple service en ligne – nous avons aspiré à créer une communauté florissante, unissant des individus partageant la même passion pour la nature.
-                <br><br>
-                En tant que fervents défenseurs de la biodiversité domestique, nous nous engageons à fournir une plateforme qui va au-delà des simples transactions. Nous avons créé un espace où chaque plante a une histoire, où chaque gardien partage un amour authentique pour la verdure. Notre engagement se manifeste dans la rigueur de nos critères de sélection pour les gardiens, assurant ainsi que seules des mains expertes prennent soin des plantes confiées.
-                <br><br>
-                La transparence est un autre pilier fondamental de notre engagement. Nous avons mis en place des outils pour faciliter la communication ouverte entre les propriétaires de plantes et les gardiens, établissant ainsi une confiance mutuelle. Nous croyons que la transparence est essentielle pour construire des relations solides au sein de notre communauté.
-                <br><br>
-                En tant qu'acteurs de la préservation de la biodiversité domestique, nous nous engageons également à sensibiliser notre communauté aux pratiques éco-responsables. Des conseils sur l'arrosage conscient aux astuces pour favoriser une croissance saine, notre site se veut être une ressource éducative pour tous les membres de notre communauté.
             </p>
-
         </div>
     </div>
 </section>
